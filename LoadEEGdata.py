d8ad9c9018320f0e80f124d1972c529ce9d41c21
Quincy Ma <mangomqy@gmail.com>
2023-04-08 15:00:56 +0800

    20230408

---
+++ LoadEEGdata.py
@@ -0,0 +1,21 @@
+
+'''
+Load EEG data 
+
+'''
+import scipy.io as sio
+import numpy as np
+
+
+class LoadEEGdata:
+    def __init__(self):
+        pass
+
+    def load_data(self,filename):
+        data = sio.loadmat(filename)['data']
+        return data
+    
+if __name__ == "__main__":
+    LoadEEGData = LoadEEGdata()
+    data = LoadEEGData.load_data('S1.mat')
+    print(np.shape(data))
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # EEG-Processing-ML
 ## Introduction
 The repository shows EEE-signal processing with EEG.
+
+The signals were processed with Tensorflow2.
